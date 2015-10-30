cede5a648d2f6fb1676bac6f8fe4787265a5e94a
kunikiya <kunikiya@home6.kunikiya.jp>
2015-10-30 14:17:15 +0900

    秘密鍵を実行時に読み込むのをやめた

--- ansible-play.sh
+++ ansible-play.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-eval `ssh-agent`
-ssh-add ../.ssh/ansible_private_key
-ansible-playbook -i hosts/onamae1.kunikiya.jp playbook/cent7.yml --private-key="/home/kunikiya/.ssh/ansible_private_key" -K
+#eval `ssh-agent`
+#ssh-add ../.ssh/../ansible_private_key
+#ansible-playbook -i hosts/onamae1.kunikiya.jp playbook/cent7.yml --private-key="/home/kunikiya/.ssh/ansible_private_key" -K
+ansible-playbook -i hosts/onamae1.kunikiya.jp playbook/cent7.yml
